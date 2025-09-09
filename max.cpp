27103a4bf146f32cef7e970bbe507eb571dee672
mariamqndl-create <mariamqndl@gmail.com>
2025-09-09 16:19:29 +0300

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;   
+    int maxNum = -1; 
+
+    for (int i = 0; i < N; i++) {
+        long long x;   
+        cin >> x;
+        if (x > maxNum) {
+            maxNum = x;
+        }
+    }
+
+    cout << maxNum << endl;
+    return 0;
+}
